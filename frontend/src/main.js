2d5e192015b49c4f159901416431d3da2526ee5b
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2022-11-17 23:53:31 +0100

    test cd

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import router from "@/router"
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
-import App from './App.vue'
+import App2 from './App2.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
 new Vue({
   router,
-  render: h => h(App),
+  render: h => h(App2),
 }).$mount('#app')
