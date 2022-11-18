31b21ad1b25b53ccd7ef5983193be6ed4ac263e2
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2022-11-18 23:40:10 +0100

    poup works, replacing js elements with components

--- frontend/src/components/popup/Popup.vue
+++ frontend/src/components/popup/Popup.vue
@@ -1,289 +1,103 @@
-<!-- <template>
-    <div class="container">
-      <h1 class="title">Wiadomość</h1>
-    
-      <br>
-  
-      <form @submit.prevent="submitForm">
-        <b-field label="Email">
-            <b-input type="email" v-model="email"
-                maxlength="30"
-                required>
-            </b-input>
-        </b-field>
-  
-        <b-field label="Tytuł">
-            <b-input v-model="emailTitle"
-                maxlength="60" 
-                required>
-            </b-input>
-        </b-field>
-  
-        <b-field label="Treść">
-            <b-input type="textarea" v-model="message"
-                maxlength="600"
-                required>
-            </b-input>
-        </b-field>
-  
-        <br>
-        
-        <button id="btnSendEmail" class="button">WYŚLIJ WIADOMOŚĆ</button>
-      </form> 
-  
-    </div>
-</template>
-  
-<script>
-//import axios from 'axios'
-//import { toast } from 'bulma-toast'
-
-export default {
-  name: "Popup",
-  data() {
-    return {
-      email: '',
-      emailTitle: '',
-      message: '',
-    }
-  },
-  methods: {
-    submitForm() {
-
-    }
-  }
-}
-
-</script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-.container {
-  margin: 0 auto;
-  max-width: 320px;
-}
-
-.title {
-  text-align: center;
-}
-
-#btnSendEmail {
-  display: flex;
-  width: 100%;
-  background-color: #7957d5;
-  border-color: transparent;
-  font-weight: bold;
-  color: white;
-  transition: .3s;
-}
-
-#btnSendEmail:hover {
-  background-color: #7957d5;
-  box-shadow: 0 0 5px #7957d5;
-}
-
-</style> -->
-
-<!-- <script>
-export default {
-  name: "Popup",
-  props: {
-    show: Boolean
-  },
-  data() {
-    return {
-      email: '',
-      emailTitle: '',
-      message: '',
-    }
-  },
-  methods: {
-    submitForm() {
-
-    }
-  },
-}
-</script>
-
 <template>
-  <Transition name="modal">
-    <div v-if="show" class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header">default header</slot>
-          </div>
-
-          <div class="modal-body">
-            <slot name="body">default body</slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-              <button
-                class="modal-default-button"
-                @click="$emit('close')"
-              >OK</button>
-            </slot>
-          </div>
+    <transition name="modal">
+        <div class="modal__wrapper" @click="$emit('close')">
+            <div class="modal-content" @click.stop="">
+                <!-- header -->
+                <div class="modal-header">
+                    <span class="modal-title"> {{ title }} </span>
+                    <span class="button-close" @click="$emit('close')">×</span>
+                </div>
+                <!-- body -->
+                <div class="modal-body">
+                    <slot name="body">Default body</slot>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </Transition>
+    </transition>
 </template>
 
+<script>
+export default {
+    name: "Popup",
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    mounted () {
+        document.body.addEventListener('keyup', e => {
+            if (e.key === 'Escape') this.$emit('close')
+        })
+    },
+    computed: {},
+    methods: {}
+}
+</script>
+
 <style>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
+/* animations */
+.modal-enter, .modal-leave-active {
+    opacity: 0;
 }
 
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
+.modal-enter .modal-content,
+.modal-leave-active .modal-content {
+    transform: scale(1.2);
 }
 
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
+.modal__wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    transition: opacity .2s ease;
+    right: 0;
+    z-index: 998;
+    background-color: rgba(00,00,00,.48);
 }
 
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
+.modal-content {
+    position: relative;
+    max-width: 600px;
+    padding: 20px 18px;
+    background-color: #fff;
+    border: 1px solid #dcdfe6;
+    transition: all .2s ease;
+    border-radius: 8px;
+    z-index: 999;
+    overflow: hidden;
+    
+}
+
+.modal-header {
+    display: flex;
+    align-self: center;
+    justify-content: space-between;
+    padding-bottom: 20px;
+}
+
+.modal-header span {
+    font-size: 24px;
+}
+
+.modal-header .button-close {
+    cursor: pointer;
 }
 
 .modal-body {
-  margin: 20px 0;
+    text-align: center;
 }
 
-.modal-default-button {
-  float: right;
+.modal-bottom {
+    padding-top: 40px;
 }
 
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter-from {
-  opacity: 0;
+a.another-modal {
+    color: #000;
+    border-bottom: 1px solid #000;
 }
-
-.modal-leave-to {
-  opacity: 0;
-}
-
-.modal-enter-from .modal-container,
-.modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-</style> -->
-
-
-<template>
-  <modal
-      title="Wiadomość"
-      @close="$emit('close')">
-      <div slot="body">
-
-        <!-- <form @submit.prevent="submitForm"> -->
-        <form @submit.prevent="onSubmit">
-          <b-field label="Email">
-              <b-input type="email" v-model="email"
-                  maxlength="30"
-                  required>
-              </b-input>
-          </b-field>
-
-          <b-field label="Tytuł">
-              <b-input v-model="emailTitle"
-                  maxlength="60" 
-                  required>
-              </b-input>
-          </b-field>
-
-          <b-field label="Treść">
-              <b-input type="textarea" v-model="message"
-                  maxlength="600"
-                  required>
-              </b-input>
-          </b-field>
-
-          <br>
-          
-          <button id="btnSendEmail" class="button">WYŚLIJ WIADOMOŚĆ</button>
-        </form>
-      </div>
-  </modal>
-</template>
-
-<script>
-import modal from '@/components/popup/Modal.vue'
-
-export default {
-  name: "Popup",
-  components: {
-    modal,
-  },
-  data () {
-      return {
-        email: '',
-        emailTitle: '',
-        message: '',
-      }
-  },
-  methods: {
-      onSubmit () {
-          this.$v.$touch()
-          if(!this.$v.$invalid) {
-              const user = {
-                  email: this.email,
-                  emailTitle: this.emailTitle,
-                  message: this.message,
-              }
-              console.log(user)
-
-              //DONE
-              this.email = ''
-              this.emailTitle = ''
-              this.message = ''
-              this.$v.$reset()
-              this.$emit('close')
-          }
-      }
-  }
-}
-</script>
-
-<style>
-/* .title {
-  line-height: .8;
-  margin-bottom: 0px;
-} */
-
-
 </style>
