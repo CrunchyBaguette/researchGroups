3789b07912f0413a4dbe6734e7284a56e15b10f8
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2022-11-20 20:44:41 +0100

    commit1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,15 @@ import store from "@/store"
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import App from './App.vue'
+//import { marked } from 'marked'
+
+// const markedMixin = {
+//   methods: {
+//     marked: function (input) {
+//       return marked(input);
+//     }
+//   }
+// };
 
 Vue.config.productionTip = false
 
@@ -13,3 +22,11 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app')
+
+// Vue.use(Buefy)
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App),
+// }).$mixin(markedMixin).$mount('#app')
+
