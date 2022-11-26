8ecd7c3cff305e5e881c1999e0cbb4794b7d8d96
Kamil-Lontkowski <kamillont14@gmail.com>
2022-11-26 19:40:33 +0100

    permissions for Announcements

---
+++ backend/announcements/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from backend.announcements.models import Announcement
+
+
+class IsAnnouncementAuthor(BasePermission):
+    """Grant permission to author of a announcement"""
+
+    def has_object_permission(self, request: Request, view: APIView, obj: Announcement) -> bool:
+        is_author = obj.author == request.user
+        return is_author
