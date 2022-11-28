69304a6a7f7d75ae00ef3479ed10e552754909fb
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2022-11-28 17:34:44 +0100

    pagination for announcements

--- backend/announcements/views.py
+++ backend/announcements/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from backend.announcements.models import Announcement
 from backend.announcements.serializers import AnnouncementSerializer
 
-# Create your views here.
+
+class StandardPagination(PageNumberPagination):
+    page_size = 5
+
+
 class AnnouncementViewSet(viewsets.ModelViewSet):
     queryset = Announcement.objects.all()
+    pagination_class = StandardPagination
     serializer_class = AnnouncementSerializer
