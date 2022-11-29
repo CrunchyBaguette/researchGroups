bdf2abf20612e7b86ea7fe49b852f5f618eca995
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2022-11-29 21:58:55 +0100

    formating fix

--- backend/announcements/models.py
+++ backend/announcements/models.py
@@ -14,6 +14,4 @@ class Announcement(models.Model):
     class Type(models.TextChoices):
         DEFAULT = "def", "Default"
 
-    ann_type = models.CharField(
-        max_length=20, choices=Type.choices, default=Type.DEFAULT
-    )
+    ann_type = models.CharField(max_length=20, choices=Type.choices, default=Type.DEFAULT)
