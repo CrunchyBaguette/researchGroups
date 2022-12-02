9f6b118b2ced6b972b9140f2f407a57c42143003
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2022-12-02 15:32:15 +0100

    some styling

--- frontend/src/components/announcements/addAnnouncement.vue
+++ frontend/src/components/announcements/addAnnouncement.vue
@@ -1,71 +1,65 @@
 <template>
-    <div id="content" style="display: flex; flex-flow: column; margin 0">
-      <div style="flex: 0 1 auto">
-        <p class="title" style="width: fit-content">Tworzenie ogłoszenia</p>
-      </div>
-      <div style="flex: 1 1 auto; overflow: auto; padding: 10px">
-        <div class="columns" style="width: 80%; margin: 50px auto">
-          <div class="column" style="display: flex; flex-flow: column; margin: 0">
-            <div class="box" style="flex: 1 1 auto">
-              <b-field
-                :message="!titleGiven ? 'Proszę podać tytuł ogłoszenia' : ''"
-                :type="!titleGiven ? 'is-danger' : ''"
-              >
-                <template #label
-                  ><p style="font-size: 20px">Tytuł ogłoszenia</p></template
-                >
-                <b-input
-                  @focus="titleGiven = true"
-                  v-model="announcementTitle"
-                  style="width: 100%"
-                />
-              </b-field>
-              <b-field
-                :message="!categoryGiven ? 'Proszę podać kategorię ogłoszenia' : ''"
-                :type="!categoryGiven ? 'is-danger' : ''"
-              >
-                <template #label
-                  ><p style="font-size: 20px">Kategoria</p></template
-                >
-                <b-select
-                  @focus="categoryGiven = true"
-                  v-model="announcementCategory"
-                  expanded
-                  style="width: 100%"
-                  placeholder="Wybierz kategorię"
-                >
-                  <option value="sponsor">Poszukiwanie sponsora</option>
-                  <option value="rekrutacja">Poszukiwanie nowych członków</option>
-                  <option value="projekt">Poszukiwanie osób do projektu</option>
-                </b-select>
-              </b-field>
-              <b-field
-                :message="!contentGiven ? 'Proszę podać treść ogłoszenia' : ''"
-                :type="!contentGiven ? 'is-danger' : ''"
-              >
-                <template #label
-                  ><p style="font-size: 20px">Treść ogłoszenia</p></template
-                >
-                <b-input
-                  @focus="contentGiven = true"
-                  v-model="announcementContent"
-                  style="width: 100%"
-                  type="textarea"
-                />
-              </b-field>
-            </div>
-            <div style="flex: 0 1 auto">
-              <b-button
-                style="margin-top: 50px"
-                expanded
-                type="is-success"
-                @click="clicked()"
-              >
-                Stwórz ogłoszenie
-              </b-button>
-            </div>
-          </div>
+  <div id="content">
+    <p class="title" style="width: fit-content">Tworzenie ogłoszenia</p>
+      <div id="container" style="margin: 70px auto; width: 70%;">
+        <div class="box">
+          <b-field
+            :message="!titleGiven ? 'Proszę podać tytuł ogłoszenia' : ''"
+            :type="!titleGiven ? 'is-danger' : ''"
+          >
+            <template #label
+              ><p style="font-size: 20px">Tytuł ogłoszenia</p></template
+            >
+            <b-input
+              @focus="titleGiven = true"
+              v-model="announcementTitle"
+              style="width: 100%"
+              maxlength="120"
+            />
+          </b-field>
+          <b-field
+            :message="!categoryGiven ? 'Proszę podać kategorię ogłoszenia' : ''"
+            :type="!categoryGiven ? 'is-danger' : ''"
+          >
+            <template #label
+              ><p style="font-size: 20px">Kategoria</p></template
+            >
+            <b-select
+              @focus="categoryGiven = true"
+              v-model="announcementCategory"
+              expanded
+              style="width: 100%"
+              placeholder="Wybierz kategorię"
+            >
+              <option value="sponsor">Poszukiwanie sponsora</option>
+              <option value="rekrutacja">Poszukiwanie nowych członków</option>
+              <option value="projekt">Poszukiwanie osób do projektu</option>
+            </b-select>
+          </b-field>
+          <br>
+          <b-field
+            :message="!contentGiven ? 'Proszę podać treść ogłoszenia' : ''"
+            :type="!contentGiven ? 'is-danger' : ''"
+          >
+            <template #label
+              ><p style="font-size: 20px">Treść ogłoszenia</p></template
+            >
+            <b-input
+              @focus="contentGiven = true"
+              v-model="announcementContent"
+              style="width: 100%"
+              type="textarea"
+            />
+          </b-field>
         </div>
+        <b-button
+          style="margin-top: 50px;"
+          expanded
+          type="is-success"
+          @click="clicked()"
+        >
+          Stwórz ogłoszenie
+        </b-button>
       </div>
     </div>
   </template>
@@ -80,8 +74,6 @@
       return {
         announcementTitle: "",
         announcementContent: "",
-        announcementAuthor: "", // chyba do wywalenia
-        announcementDate: "", // - // -
         announcementCategory: "",
         titleGiven: true,
         contentGiven: true,
@@ -130,7 +122,6 @@
           this.announcementTitle = "";
           this.announcementContent = "";
           this.announcementCategory = "";
-          //inne chyba nie
         }
       },
     },
