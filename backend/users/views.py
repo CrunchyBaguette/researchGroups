7296ac820fcc64b780b56057e32fb3aae005e061
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2022-12-10 00:43:32 +0100

    formaters fix

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.decorators import action
