1a93bf73b28e43355a9c7e1dd7c30096eb968526
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2022-12-10 20:08:45 +0100

    few changes

--- backend/announcements/views.py
+++ backend/announcements/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from backend.announcements.models import Announcement
 from backend.announcements.serializers import AnnouncementSerializer
 from backend.common.views import PermissionPolicyMixin
@@ -8,6 +8,7 @@ from backend.common.views import PermissionPolicyMixin
 class AnnouncementViewSet(PermissionPolicyMixin, viewsets.ModelViewSet):
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
+    permission_classes = [AllowAny]
     permission_classes_per_method = {
         "create": [
             IsAuthenticated,
