ed7182f7015a5dcc50894bbcace448b350767dac
Kamil-Lontkowski <kamillont14@gmail.com>
2022-12-13 22:21:51 +0100

    progress

--- backend/tutorials/serializers.py
+++ backend/tutorials/serializers.py
@@ -5,16 +5,18 @@ from backend.tutorials.models import Tutorial, Rating
 from backend.utilsx.serializers import QuerySerializerMixin
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    owner = UserSerializer()
-    editors = UserSerializer(many=True)
+class TutorialSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(partial=True)
+    editors = UserSerializer(many=True, partial=True)
+    editable = serializers.BooleanField(read_only=True, default=False)
 
     class Meta:
         model = Tutorial
         fields = "__all__"
+        read_only_fields = ["created", "edited", "editable"]
 
 
-class RatingTutorial(QuerySerializerMixin, serializers.ModelSerializer):
+class RatingTutorialSerializer(QuerySerializerMixin, serializers.ModelSerializer):
     author = serializers.SlugRelatedField(slug_field="username", queryset=User.objects.all())
 
     RELATED_FIELD = ["author"]
@@ -22,3 +24,13 @@ class RatingTutorial(QuerySerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = "__all__"
+
+
+class AddEditorsSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    username = serializers.CharField(required=False)
+    email = serializers.SlugRelatedField(slug_field="email", queryset=User.objects.all())
+
+    class Meta:
+        fields = "__all__"
+        write_only_fields = ["id", "username", "email"]
--- backend/tutorials/views.py
+++ backend/tutorials/views.py
@@ -1,2 +1,30 @@
+from typing import Any
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from backend.projects.models import Project
+from backend.users.views import PermissionPolicyMixin
 from backend.tutorials.models import Tutorial, Rating
+from backend.tutorials.serializers import TutorialSerializer
+from backend.tutorials.permissions import IsTutorialEditor, IsTutorialOwner
+
+
+class TutorialViewSet(PermissionPolicyMixin, viewsets.ModelViewSet):
+    serializer_class = TutorialSerializer
+    queryset = Tutorial.objects.all().order_by("created")
+    permission_classes = [AllowAny]
+    permission_classes_per_method = {
+        "create": [IsAuthenticated],
+        "partial_update": [IsTutorialEditor | IsTutorialOwner],
+        "update": [IsTutorialEditor | IsTutorialOwner],
+        "destroy": [IsTutorialOwner]
+    }
+
+    def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        pass
+
+    @action()
+    def add_editors(self):
+        pass
